0051bfe2677ab2ae0c8965533432cd087429dea4
jesperante <129589499+jesperante@users.noreply.github.com>
2023-04-06 21:47:44 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,13 +5,13 @@ node default {
       owner => 'root',
     }
 }
-node 'master.puppet.vm'{ 
+node 'master.puppet.vm' { 
    include role::master_server 
-}
-node /^web/{
+ }
+node /^web/ {
    include role::app_server
 }
 
-node /^db/{
+node /^db/ {
    include role::db_server
 }
