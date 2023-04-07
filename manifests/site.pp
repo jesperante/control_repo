54157d610ea8ec06e65dc6488d2c341bfc079dc3
jesperante <129589499+jesperante@users.noreply.github.com>
2023-04-07 10:59:17 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,12 +6,12 @@ node default {
     }
 }
 node master.puppet.vm { 
-   include role::master_server 
+   include role 'master_server' 
  }
 node /^web/ {
-   include role::app_server
+   include role 'app_server'
 }
 
 node /^db/ {
-   include role::db_server
+   include role 'db_server'
 }
